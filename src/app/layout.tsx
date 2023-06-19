c030a6c345963093776ae6782c272b83491d83f1
Lawson Chibueze <speak2lawson@gmail.com>
2023-06-19 20:18:11 +0100

    added aws-export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 import { Amplify } from "aws-amplify";
 
-import awsconfig from "../aws-exports";
+import awsconfig from "../../src/aws-exports";
 
 Amplify.configure({ ...awsconfig, ssr: true });
 
